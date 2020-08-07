b0357a9f3b9e7784050fcf529cc86e04d7dde572
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-07 11:07:19 -0400

    Setup reducer and store

---
+++ app/src/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+    anime: null,
+    animes: []
+}
+
+export const reducer = (state = initialState, action) =>{
+    switch (action.type){
+        default:
+            return state;
+    }
+}
