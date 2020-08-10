5c8a4093661009efa5cf7e9231fb63850a53879b
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-10 11:45:33 -0400

    Added link to homepage on logo in nav

--- app/src/components/Nav/index.jsx
+++ app/src/components/Nav/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 import logo from "../../images/logo.svg";
 
 const Nav = (props) => {
     return (
         <nav>
-            <div className="logo">
+            <Link to="/" className="logo">
                 <img width="45" src={logo}/>
                 <div> <span>Gaijin</span> Goods</div>
-            </div>
+            </Link>
             <div className="links">
 
             </div>
