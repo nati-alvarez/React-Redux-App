3b32452921eedab0b7669b83fafe3cffe1214d0e
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-15 12:04:05 -0400

    Added search form to homepage

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import TopAnime from "./TopAnime";
+import SearchForm from "../SearchForm";
 
 const Home = props =>{
     return (
         <div className="home-page">
+            <SearchForm/>
             <TopAnime/>
         </div>
     )
