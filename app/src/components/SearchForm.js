27bd8fcbd4e40acfe6dcd5fdaf02565188bce2ea
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-15 12:17:09 -0400

    Search page redirects to search results page on submit

--- app/src/components/SearchForm.js
+++ app/src/components/SearchForm.js
@@ -1,10 +1,15 @@
 import React from "react";
+import {useHistory} from "react-router-dom";
 import {useForm} from "../hooks/useForm";
 
 const SearchForm = props => {
+    const history = useHistory();
     const initialValues = {query: ""}
     const [formData, handleChange, handleSubmit] = useForm(initialValues, submit);
     
+    function submit(){
+        history.push(`/search-results?query=${formData.query}`);
+    }
     
     return (
         <form onSubmit={handleSubmit}>
